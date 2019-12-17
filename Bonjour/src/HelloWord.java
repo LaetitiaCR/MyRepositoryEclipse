5c2b4863410b602a905a95bf978a5ad7c4a248d0
LaetitiaCR <l.carrivale@laposte.net>
2019-12-17 10:00:43 +0100

    modification

--- Bonjour/src/HelloWord.java
+++ Bonjour/src/HelloWord.java
@@ -1,11 +1,17 @@
-
-public class HelloWord {
-	
-	/*
-	*
+/**
+	*@author Laëtitia
+	*@version 2019
 	*@param args arguments
 	*/
+public class HelloWord {
+	
+	
 	public static void main(String[] args) {
+		
+		/**
+		* @author Laëti
+		* @version  
+		*/
 		
 		System.out.println("Hello World !!!");
 	}
