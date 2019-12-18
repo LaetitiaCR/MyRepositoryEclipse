49b2c15bc5226bcb7a910e154ed8b4615fdd2be8
LaetitiaCR <l.carrivale@laposte.net>
2019-12-18 12:17:18 +0100

    mise à jour

---
+++ Bonjour/test/com/crm/formation/StringTest.java
@@ -0,0 +1,41 @@
+package com.crm.formation;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class StringTest {
+
+	/**
+	 * @param args
+	 */
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		//strScTexte.concat(strScTexte);
+		
+		
+		
+		
+		String texte;
+		texte = "Bonjour";
+		
+		System.out.println(texte);
+		
+		texte = texte + " Hello";
+		
+		System.out.println(texte);
+	
+	}
+
+}
